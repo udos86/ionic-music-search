69beaa6e0b78672dcde88eed27793d595d24eab1
Udo Schöfer <developer@udos86.de>
2021-12-16 13:52:47 +0100

    removes unused imports

--- src/app/detail/detail-routing.module.ts
+++ src/app/detail/detail-routing.module.ts
@@ -1,6 +1,5 @@
-import { NgModule, OnInit } from '@angular/core';
-import { Routes, RouterModule, ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { DetailPage } from './detail.page';
 
